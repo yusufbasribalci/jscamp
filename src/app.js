cf22eb8225b9b10a5a6383ebd69495dbd115e929
yusufbasribalci <yusufbasribalci@gmail.com>
2021-10-30 17:26:06 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log("merhaba ben volkan konak")
+
+let dolarDun = 4.32
+let konutKredileri =["wwa","dawfwa","edwd"]
+console.log("<ul>") 
+
+for (let i = 0; i < 3;  i++) {
+    console.log ("<li>"+konutKredileri[i]+"</li>")
+   
+    
+}
+console.log("</ul>")
